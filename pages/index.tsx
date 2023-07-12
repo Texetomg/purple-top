7d684a3b877c23abfd14a7450b7f3378376558d1
Bogdan Chentsov <texetomg@gmail.com>
2023-07-12 10:08:38 +0300

    Init.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+import styles from '@/styles/Home.module.css';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
+  const a = 1;
   return (
     <>
       <Head>
@@ -104,11 +105,11 @@ export default function Home() {
             </h2>
             <p>
               Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
+              with&nbsp;Vercel.{a}
             </p>
           </a>
         </div>
       </main>
     </>
-  )
+  );
 }
