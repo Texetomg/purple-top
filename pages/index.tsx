1c640e20218d51cf831d53695b29df691d3bfcca
Bogdan Chentsov <texetomg@gmail.com>
2023-08-18 09:08:52 +0300

    Added components.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,26 @@
+import { Button, Htag, P } from '@/components';
+
 export default function Home(): JSX.Element {
   return (
-    <div>asdaskdlmasf  </div>
+    <div>
+      <Htag tag='h1'>
+        Текст
+      </Htag> 
+      <Button appearance='primary' arrow='right'>
+        Кнопка
+      </Button>
+       <Button appearance='ghost' arrow='down'>
+        Кнопка
+      </Button>
+      <P size='l'>
+        Большой
+      </P>
+      <P>
+        Средний
+      </P>
+      <P size='s'>
+        Маленький
+      </P>
+    </div>
   );
 }
