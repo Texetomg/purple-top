e392cf5c868086704a04ce8651081af246a69fdf
Bogdan Chentsov <texetomg@gmail.com>
2023-09-14 08:11:55 +0300

    Added sidebar.

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { SidebarProps } from './Sidebar.props';
 import cn from 'classnames';
-import styles from './P.module.css';
+import styles from './Sidebar.module.css';
 import { Menu } from '../Menu/Menu';
+import Logo from '../logo.svg';
 
-export const Sidebar = ({  ...props }: SidebarProps): JSX.Element => (
-  <div {...props}>
+export const Sidebar = ({ className, ...props }: SidebarProps): JSX.Element => (
+  <div className={cn(className, styles.sidebar)} {...props}>
+    <Logo className={styles.logo}/>
+    <div>Поиск</div>
     <Menu />
   </div>
 );
