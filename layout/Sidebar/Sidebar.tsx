db792958b5b2bb4ef29035754fd8d9d747df1b2e
Bogdan Chentsov <texetomg@gmail.com>
2023-09-16 11:05:34 +0300

    Added search form.

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -4,11 +4,13 @@ import cn from 'classnames';
 import styles from './Sidebar.module.css';
 import { Menu } from '../Menu/Menu';
 import Logo from '../logo.svg';
+import { Search } from '@/components';
+
 
 export const Sidebar = ({ className, ...props }: SidebarProps): JSX.Element => (
   <div className={cn(className, styles.sidebar)} {...props}>
     <Logo className={styles.logo}/>
-    <div>Поиск</div>
+    <Search />
     <Menu />
   </div>
 );
