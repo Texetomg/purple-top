922ac43a243ca5a4515ea391e5cabd419009ab13
Bogdan Chentsov <texetomg@gmail.com>
2023-09-19 10:56:30 +0300

    Fixed reducer.

--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -3,14 +3,19 @@ import { TopPageComponentProps } from './TopPageComponent.props';
 import styles from './TopPageComponent.module.css';
 import { TopLevelCategory } from '@/interfaces/page.interface';
 import { SortEnum } from '@/components/Sort/Sort.props';
-import { useReducer } from 'react';
+import { useEffect, useReducer } from 'react';
 import { sortReducer } from './sort.reducer';
 
 export const TopPageComponent = ({ page, products, firstCategory}: TopPageComponentProps): JSX.Element => {
   const [{ products: sortedProducts, sort }, dispatchSort] = useReducer(sortReducer, { products, sort: SortEnum.Rating});
+  
   const setSort = (sort: SortEnum) => {
     dispatchSort({ type: sort});
   };
+
+  useEffect(() => {
+    dispatchSort({ type: 'reset', initialState: products});
+  }, [products]);
 
   return (
    <div className={styles.wrapper}>
