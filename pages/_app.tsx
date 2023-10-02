547ffb98edc530330b49479c612ea7949f1290bb
Ченцов Богдан Вячеславович <bvyachentsov@sberbank.ru>
2023-10-02 09:59:00 +0300

    Added meta.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,31 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 
-export default function App({ Component, pageProps }: AppProps): JSX.Element {
+export default function App({ Component, pageProps, router }: AppProps): JSX.Element {
   return (
     <>
       <Head>
         <title>My top - наш лучший топ</title>
-        <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com"/>
-        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;700&display=swap" rel="stylesheet"/>
+        <link
+          rel="preconnect"
+          href="https://fonts.googleapis.com"
+        />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;700&display=swap"
+          rel="stylesheet"
+        />
+        <meta
+          property='og:url'
+          content={process.env.NEXT_PUBLIC_DOMAIN + router.asPath}
+        />
+        <meta
+          property='og:locale'
+          content="ru_RU"
+        />
       </Head>
       <Component {...pageProps} />
     </>
